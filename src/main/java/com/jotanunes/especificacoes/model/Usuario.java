32378d19bdd5c196dc11c1417cf871f963c0fde0
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-09-21 20:35:31 -0300

    Ajuste da entidade usuario e enum de Roles

--- src/main/java/com/jotanunes/especificacoes/model/Usuario.java
+++ src/main/java/com/jotanunes/especificacoes/model/Usuario.java
@@ -1,28 +1,33 @@
 package com.jotanunes.especificacoes.model;
 
 
+import com.jotanunes.especificacoes.enums.NivelAcesso;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "usuario")
-public class Usuario {
+@Table(name = "usuarios")
+public class Usuario implements UserDetails {
 
     @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(nullable = false, unique = true, length = 80)
+    @Column(nullable = false, unique = true, length = 30)
     private String nome;
     @Column(nullable = false)
     private String senha;
     @Column(name = "nivel_acesso", nullable = false, length = 10)
-    private String nivelAcesso;
+    private NivelAcesso nivelAcesso;
     @Column(nullable = false, unique = true, length = 255)
     private String email;
     @Column(name = "data_criacao", nullable = false)
@@ -37,80 +42,45 @@ public class Usuario {
     public Usuario() {
     }
 
-    public Usuario(UUID id, String nome, String senha, String nivelAcesso, String email, LocalDateTime dataCriacao, Usuario criadoPor, Boolean ativo) {
-        this.id = id;
-        this.nome = nome;
-        this.senha = senha;
-        this.nivelAcesso = nivelAcesso;
-        this.email = email;
-        this.dataCriacao = dataCriacao;
-        this.criadoPor = criadoPor;
-        this.ativo = ativo;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.nivelAcesso == NivelAcesso.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN")
+                , new SimpleGrantedAuthority("ROLE_GESTOR"),
+                new SimpleGrantedAuthority("ROLE_PADRAO"));
+        else if (this.nivelAcesso == NivelAcesso.GESTOR) return List.of(new SimpleGrantedAuthority("ROLE_GESTOR")
+        , new SimpleGrantedAuthority("ROLE_PADRAO"));
+
+        return List.of(new SimpleGrantedAuthority("ROLE_PADRAO"));
     }
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSenha() {
+    @Override
+    public String getPassword() {
         return senha;
     }
 
-    public void setSenha(String senha) {
-        this.senha = senha;
+    @Override
+    public String getUsername() {
+        return nome;
     }
 
-    public String getNivelAcesso() {
-        return nivelAcesso;
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
     }
 
-    public void setNivelAcesso(String nivelAcesso) {
-        this.nivelAcesso = nivelAcesso;
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
     }
 
-    public String getEmail() {
-        return email;
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
-
-    public LocalDateTime getDataCriacao() {
-        return dataCriacao;
-    }
-
-    public void setDataCriacao(LocalDateTime dataCriacao) {
-        this.dataCriacao = dataCriacao;
-    }
-
-    public Usuario getCriadoPor() {
-        return criadoPor;
-    }
-
-    public void setCriadoPor(Usuario criadoPor) {
-        this.criadoPor = criadoPor;
-    }
-
-    public Boolean getAtivo() {
-        return ativo;
-    }
-
-    public void setAtivo(Boolean ativo) {
-        this.ativo = ativo;
-    }
-
-
 }
