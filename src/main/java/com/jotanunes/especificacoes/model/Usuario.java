6911fcb0c3aff659ada9e346f923f175f011b915
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-09-21 20:48:37 -0300

    Getters and Setters de usuario(esqueci)

--- src/main/java/com/jotanunes/especificacoes/model/Usuario.java
+++ src/main/java/com/jotanunes/especificacoes/model/Usuario.java
@@ -42,6 +42,84 @@ public class Usuario implements UserDetails {
     public Usuario() {
     }
 
+    public Usuario(String nome, String senha, NivelAcesso nivelAcesso, String email, LocalDateTime dataCriacao, Usuario criadoPor, Boolean ativo) {
+        this.nome = nome;
+        this.senha = senha;
+        this.nivelAcesso = nivelAcesso;
+        this.email = email;
+        this.dataCriacao = dataCriacao;
+        this.criadoPor = criadoPor;
+        this.ativo = ativo;
+    }
+
+    public Usuario(String nome, String senha, NivelAcesso nivelAcesso, String email, LocalDateTime dataCriacao, Boolean ativo) {
+        this.nome = nome;
+        this.senha = senha;
+        this.nivelAcesso = nivelAcesso;
+        this.email = email;
+        this.dataCriacao = dataCriacao;
+        this.ativo = ativo;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public NivelAcesso getNivelAcesso() {
+        return nivelAcesso;
+    }
+
+    public void setNivelAcesso(NivelAcesso nivelAcesso) {
+        this.nivelAcesso = nivelAcesso;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Usuario getCriadoPor() {
+        return criadoPor;
+    }
+
+    public void setCriadoPor(Usuario criadoPor) {
+        this.criadoPor = criadoPor;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
