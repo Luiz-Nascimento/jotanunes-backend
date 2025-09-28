6f9f045204244db2d09b38ce32b660733024bd63
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-09-28 16:02:24 -0300

    Changed Handler Annotation to RestControllerAdvice

--- src/main/java/com/jotanunes/especificacoes/exception/handler/CustomEntityResponseHandler.java
+++ src/main/java/com/jotanunes/especificacoes/exception/handler/CustomEntityResponseHandler.java
@@ -3,16 +3,15 @@ package com.jotanunes.especificacoes.exception.handler;
 import com.jotanunes.especificacoes.exception.ExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 
-@ControllerAdvice
-@RestController
+
+@RestControllerAdvice
 public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
