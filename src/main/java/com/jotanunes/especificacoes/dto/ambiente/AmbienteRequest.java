e519555c93cf4d2689233aa534131b7746fbffa5
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-09-28 16:11:31 -0300

    Validação do AmbienteRequest

--- src/main/java/com/jotanunes/especificacoes/dto/ambiente/AmbienteRequest.java
+++ src/main/java/com/jotanunes/especificacoes/dto/ambiente/AmbienteRequest.java
@@ -1,10 +1,17 @@
 package com.jotanunes.especificacoes.dto.ambiente;
 
 import com.jotanunes.especificacoes.enums.TipoAmbiente;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record AmbienteRequest(
+        @NotNull(message = "ID do empreendimento não pode ser nulo")
         Integer idEmpreendimento,
+        @Size(max = 80)
+        @NotBlank(message = "Nome do ambiente não pode ser nulo.")
         String nome,
+        @NotNull(message = "Tipo do ambiente não pode ser nulo.")
         TipoAmbiente tipo
 ) {
 }
