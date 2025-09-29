5465c1c1048fff02f473519788dbb65083a80b20
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-09-29 15:40:05 -0300

    Repository de item

---
+++ src/main/java/com/jotanunes/especificacoes/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.jotanunes.especificacoes.repository;
+
+import com.jotanunes.especificacoes.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Integer, Item> {
+}
