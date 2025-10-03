8a879bfce4896946d7f31cd9d90dabaa6550ac22
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-10-02 23:48:25 -0300

    Adição de restrição de tamanho de nome em marcas

--- src/main/java/com/jotanunes/especificacoes/dto/marca/MarcaRequest.java
+++ src/main/java/com/jotanunes/especificacoes/dto/marca/MarcaRequest.java
@@ -1,9 +1,11 @@
 package com.jotanunes.especificacoes.dto.marca;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record MarcaRequest(
         @NotBlank(message = "O nome da marca é obrigatório")
+        @Size(max = 100, message = "O nome não pode ter mais de 100 caracteres")
         String nome
 ) {
 }
--- src/main/java/com/jotanunes/especificacoes/model/Marca.java
+++ src/main/java/com/jotanunes/especificacoes/model/Marca.java
@@ -10,7 +10,7 @@ public class Marca {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, length = 100)
     private String nome;
 
     public Marca() {
