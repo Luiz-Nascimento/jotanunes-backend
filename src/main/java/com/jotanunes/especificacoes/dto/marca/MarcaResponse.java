025d64cac81695f1fa425f5feb49b76145c9ede0
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-10-02 23:34:10 -0300

    DTO de MarcaRequest para tipo Record

--- src/main/java/com/jotanunes/especificacoes/dto/marca/MarcaResponse.java
+++ src/main/java/com/jotanunes/especificacoes/dto/marca/MarcaResponse.java
@@ -1,31 +1,3 @@
 package com.jotanunes.especificacoes.dto.marca;
 
-public class MarcaResponse {
-
-    private Integer id;
-    private String nome;
-
-    public MarcaResponse() {
-    }
-
-    public MarcaResponse(Integer id, String nome) {
-        this.id = id;
-        this.nome = nome;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-}
+public record MarcaResponse(Integer id, String nome){}
