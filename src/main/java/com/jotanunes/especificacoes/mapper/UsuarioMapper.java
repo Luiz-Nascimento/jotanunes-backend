e8b7ab917624b78dcc7a4f3139732304b36fa7dd
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-10-06 18:56:56 -0300

    Refactory no mapper de usuario

--- src/main/java/com/jotanunes/especificacoes/mapper/UsuarioMapper.java
+++ src/main/java/com/jotanunes/especificacoes/mapper/UsuarioMapper.java
@@ -1,6 +1,6 @@
 package com.jotanunes.especificacoes.mapper;
 
-import com.jotanunes.especificacoes.dto.UsuarioExibirDTO;
+import com.jotanunes.especificacoes.dto.UserResponse;
 import com.jotanunes.especificacoes.dto.auth.RegisterRequest;
 import com.jotanunes.especificacoes.model.Usuario;
 import org.mapstruct.Mapper;
@@ -9,8 +9,7 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
 
-    @Mapping(target = "id", expression = "java(usuario.getId().toString())")
-    UsuarioExibirDTO toDto(Usuario usuario);
+    UserResponse toDto(Usuario usuario);
 
     Usuario toEntity(RegisterRequest request);
 }
