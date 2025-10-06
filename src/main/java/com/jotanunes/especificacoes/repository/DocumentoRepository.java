e083de6625db481dc4422f55194c6d53caa8e472
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-10-06 20:21:21 -0300

    Criação do repository de documento

---
+++ src/main/java/com/jotanunes/especificacoes/repository/DocumentoRepository.java
@@ -0,0 +1,10 @@
+package com.jotanunes.especificacoes.repository;
+
+import com.jotanunes.especificacoes.model.Documento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocumentoRepository extends JpaRepository<Documento, Integer> {
+
+}
