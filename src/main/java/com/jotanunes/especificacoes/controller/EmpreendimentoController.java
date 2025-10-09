77120e5357d60d0c2d42fcfc08a92cf287714262
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-10-09 16:11:47 -0300

    Descrição de endpoints de empreendimento

--- src/main/java/com/jotanunes/especificacoes/controller/EmpreendimentoController.java
+++ src/main/java/com/jotanunes/especificacoes/controller/EmpreendimentoController.java
@@ -4,6 +4,11 @@ import com.jotanunes.especificacoes.dto.empreendimento.EmpreendimentoDocResponse
 import com.jotanunes.especificacoes.dto.empreendimento.EmpreendimentoRequest;
 import com.jotanunes.especificacoes.dto.empreendimento.EmpreendimentoResponse;
 import com.jotanunes.especificacoes.service.EmpreendimentoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
@@ -30,36 +35,59 @@ public class EmpreendimentoController {
         this.empreendimentoService = empreendimentoService;
     }
 
+    @Operation(
+            summary = "Buscar todos empreendimentos",
+            description = "Retorna dados de todos empreendimentos cadastrados"
+    )
     @GetMapping
     public List<EmpreendimentoResponse> findAll() {
         return empreendimentoService.findAll();
     }
 
+    @Operation(
+            summary = "Buscar empreendimento por ID",
+            description = "Retorna dados do empreendimento com ID especificado "
+    )
     @GetMapping("/{id}")
     public ResponseEntity<EmpreendimentoResponse> findById(@PathVariable Integer id) {
         EmpreendimentoResponse response = empreendimentoService.findById(id);
         return ResponseEntity.ok().body(response);
     }
 
+    @Operation(
+            summary = "Buscar dados enxutos de um empreendimento por ID",
+            description = "Retorna dados enxutos prontos para serem preenchidos num documento"
+    )
     @GetMapping("/doc/{id}")
     public ResponseEntity<EmpreendimentoDocResponse> getDocResponse(@PathVariable Integer id) {
         EmpreendimentoDocResponse response = empreendimentoService.getDocResponse(id);
         return ResponseEntity.ok().body(response);
     }
 
+    @Operation(
+            summary = "Criação de um novo empreendimento",
+            description = "Cria um novo empreendimento apartir das informações fornecidas no JSON"
+    )
     @PostMapping
     public ResponseEntity<EmpreendimentoResponse> create(@RequestBody @Valid EmpreendimentoRequest data) {
         EmpreendimentoResponse response = empreendimentoService.create(data);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    
+    @Operation(
+            summary = "Atualiza um empreendimento",
+            description = "Atualiza um empreendimento especificado com ID, apartir das informações fornecidas no JSON"
+    )
     @PutMapping("/{id}")
      public ResponseEntity<EmpreendimentoResponse> update(@PathVariable Integer id, @RequestBody @Valid EmpreendimentoRequest data) {
         EmpreendimentoResponse response = empreendimentoService.update(id, data);
         return ResponseEntity.ok().body(response); //quando o usuario quiser corrigir o documento reprovado pelo gestor
     }
 
+    @Operation(
+            summary = "Deleta um empreendimento",
+            description = "Deleta um empreendimento apartir de seu ID. Necessita de role de ADMIN"
+    )
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable Integer id) {
