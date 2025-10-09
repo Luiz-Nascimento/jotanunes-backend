a3c789c71093360476f669fdfd612e0155ffabb9
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-10-09 15:21:32 -0300

    Editei revisão de itens para ficar compatível com a tabela do banco, e removi status de empreendimento(Status do documento tem o mesmo propósito)

---
+++ src/main/java/com/jotanunes/especificacoes/model/RevisaoItem.java
@@ -0,0 +1,82 @@
+package com.jotanunes.especificacoes.model;
+
+import com.jotanunes.especificacoes.enums.ItemStatus;
+import jakarta.persistence.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "revisao_item")
+public class RevisaoItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "item_id", referencedColumnName = "id", nullable = false, unique = true)
+    private Item item;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ItemStatus status;
+
+    @Column(name = "motivo", nullable = false)
+    private String motivo;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id", nullable = false)
+    private Usuario usuario;
+
+    @UpdateTimestamp
+    @Column(name = "data_avaliacao", nullable = false)
+    private LocalDateTime dataAvaliacao;
+
+    public RevisaoItem() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public ItemStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ItemStatus status) {
+        this.status = status;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public LocalDateTime getDataAvaliacao() {
+        return dataAvaliacao;
+    }
+
+    public void setDataAvaliacao(LocalDateTime dataAvaliacao) {
+        this.dataAvaliacao = dataAvaliacao;
+    }
+}
