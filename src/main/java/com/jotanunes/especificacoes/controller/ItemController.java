aa1a35bc10bd64a3f01e8a5c3742c11c472a9c1a
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-10-09 16:54:07 -0300

    ID do ambiente no response de item, renomeação de métodos de item

--- src/main/java/com/jotanunes/especificacoes/controller/ItemController.java
+++ src/main/java/com/jotanunes/especificacoes/controller/ItemController.java
@@ -3,7 +3,6 @@ package com.jotanunes.especificacoes.controller;
 import com.jotanunes.especificacoes.dto.item.ItemDocResponse;
 import com.jotanunes.especificacoes.dto.item.ItemRequest;
 import com.jotanunes.especificacoes.dto.item.ItemResponse;
-import com.jotanunes.especificacoes.model.Item;
 import com.jotanunes.especificacoes.service.ItemService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,31 +21,31 @@ public class ItemController {
     private ItemService service;
 
     @GetMapping
-    public List<ItemResponse> findAll() {
-        return service.findAll();
+    public List<ItemResponse> listAllItens() {
+        return service.getAllItens();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ItemResponse> findById(@PathVariable Integer id) {
-        ItemResponse response = service.findById(id);
+    public ResponseEntity<ItemResponse> getItem(@PathVariable Integer id) {
+        ItemResponse response = service.getItemById(id);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/doc/{id}")
-    public ResponseEntity<ItemDocResponse> getDocResponse(@PathVariable Integer id) {
-        ItemDocResponse response = service.getDocResponse(id);
+    public ResponseEntity<ItemDocResponse> getItemDocResponse(@PathVariable Integer id) {
+        ItemDocResponse response = service.getItemDocResponse(id);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/{idAmbiente}")
-    public ResponseEntity<ItemResponse> create(@PathVariable Integer idAmbiente, @RequestBody ItemRequest data) {
-        ItemResponse response = service.create(data, idAmbiente);
+    public ResponseEntity<ItemResponse> createItem(@PathVariable Integer idAmbiente, @RequestBody ItemRequest data) {
+        ItemResponse response = service.createItem(data, idAmbiente);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Integer id) {
-        service.delete(id);
+    public ResponseEntity<Void> deleteItem(@PathVariable Integer id) {
+        service.deleteItem(id);
         return ResponseEntity.noContent().build();
     }
 }
