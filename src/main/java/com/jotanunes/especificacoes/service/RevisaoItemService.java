a883a884e16a8e321bca485980daf9a469036d0b
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-10-13 19:23:57 -0300

    Serviço para revisar vários itens numa requisição, revisar item agora recebe id do item dentro do dto, listar todas revisões

--- src/main/java/com/jotanunes/especificacoes/service/RevisaoItemService.java
+++ src/main/java/com/jotanunes/especificacoes/service/RevisaoItemService.java
@@ -1,27 +1,27 @@
 package com.jotanunes.especificacoes.service;
 
-import com.jotanunes.especificacoes.repository.ItemRepository;
+import com.jotanunes.especificacoes.dto.revisaoItens.RevisaoItemResponse;
+import com.jotanunes.especificacoes.mapper.RevisaoItemMapper;
 import com.jotanunes.especificacoes.repository.RevisaoItemRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RevisaoItemService {
 
     private final RevisaoItemRepository repository;
-    private final ItemRepository itemRepository;
 
-    public RevisaoItemService(RevisaoItemRepository repository, ItemRepository itemRepository) {
+    private final RevisaoItemMapper revisaoItemMapper;
+
+    public RevisaoItemService(RevisaoItemRepository repository, RevisaoItemMapper revisaoItemMapper) {
         this.repository = repository;
-        this.itemRepository = itemRepository;
+        this.revisaoItemMapper = revisaoItemMapper;
     }
 
-    //Revisar item
-    //Caso o status seja aprovado, atualizar o status do item para APROVADO
-    //Guardar informação do usuário que aprovou, salva a auditoria e retorna a revisão;
-    //Caso o status seja reprovado, atualizar o status do item para reprovado,
-    //Receber obrigatoriamente uma descrição do motivo da reprovação, guarda a auditoria e retorna a revisão.
-    // Retornar erro caso o item esteja revisado(APROVADO ou REPROVAD0).
-    // Retornar erro caso o item não exista.
-    // Retornar erro caso o usuário não tenha permissão para revisar o item.
+    public List<RevisaoItemResponse> findAll(){
+        return repository.findAll().stream().map(revisaoItemMapper::toDto).toList();
+    }
+
 
 }
