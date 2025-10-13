36b0b0341710f8be9ed4b475aab5e1ee1603d718
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-10-13 16:03:03 -0300

    Documentação MarcaController

--- src/main/java/com/jotanunes/especificacoes/controller/MarcaController.java
+++ src/main/java/com/jotanunes/especificacoes/controller/MarcaController.java
@@ -3,6 +3,7 @@ package com.jotanunes.especificacoes.controller;
 import com.jotanunes.especificacoes.dto.marca.MarcaRequest;
 import com.jotanunes.especificacoes.dto.marca.MarcaResponse;
 import com.jotanunes.especificacoes.service.MarcaService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -21,26 +22,46 @@ public class MarcaController {
         this.service = service;
     }
 
+    @Operation(
+            summary = "Criar uma nova marca",
+            description = "Cria uma nova marca com os dados fornecidos"
+    )
     @PostMapping
     public ResponseEntity<MarcaResponse> create(@RequestBody @Valid MarcaRequest dto) {
         return ResponseEntity.ok(service.create(dto));
     }
 
+    @Operation(
+            summary = "Retornar dados de todas as marcas",
+            description = "Retorna dados de todas as marcas cadastradas"
+    )
     @GetMapping
     public ResponseEntity<List<MarcaResponse>> findAll() {
         return ResponseEntity.ok(service.findAll());
     }
 
+    @Operation(
+            summary = "Retornar dados de uma marca",
+            description = "Retorna dados da marca com ID especificado"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<MarcaResponse> findById(@PathVariable Integer id) {
         return ResponseEntity.ok(service.findById(id));
     }
 
+    @Operation(
+            summary = "Atualizar dados de uma marca",
+            description = "Atualiza os dados da marca com ID especificado"
+    )
     @PutMapping("/{id}")
     public ResponseEntity<MarcaResponse> update(@PathVariable Integer id, @RequestBody @Valid MarcaRequest dto) {
         return ResponseEntity.ok(service.update(id, dto));
     }
 
+    @Operation(
+            summary = "Deletar uma marca",
+            description = "Deleta a marca com ID especificado"
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
