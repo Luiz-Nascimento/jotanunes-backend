2abbe1edd548004b0b1c815cd55c8264b3ab7525
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-10-13 16:09:31 -0300

    Documentação UserController, listar usuarios apenas sendo admin

--- src/main/java/com/jotanunes/especificacoes/controller/UserController.java
+++ src/main/java/com/jotanunes/especificacoes/controller/UserController.java
@@ -3,6 +3,7 @@ package com.jotanunes.especificacoes.controller;
 import com.jotanunes.especificacoes.dto.usuario.RoleChangeRequest;
 import com.jotanunes.especificacoes.dto.usuario.UserResponse;
 import com.jotanunes.especificacoes.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,11 +21,20 @@ public class UserController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Operation(
+            summary = "Listar todos os usuários",
+            description = "Retorna uma lista de todos os usuários cadastrados no sistema. Apenas administradores podem acessar essa informação."
+    )
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public List<UserResponse> findAll() {
         return usuarioService.findAll();
     }
 
+    @Operation(
+            summary = "Alterar o papel de um usuário",
+            description = "Permite alterar o papel (role) de um usuário específico. Apenas administradores podem realizar essa ação."
+    )
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}/role")
     public ResponseEntity<UserResponse> updateUserRole(@PathVariable UUID id, @RequestBody RoleChangeRequest role) {
