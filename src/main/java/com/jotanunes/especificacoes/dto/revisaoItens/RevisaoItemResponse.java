8ab5af256d86fa6e30390fa95d4674ea57b84a83
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-10-14 13:45:12 -0300

    JsonInclude notnull no campo motivo do response de RevisaoItem

--- src/main/java/com/jotanunes/especificacoes/dto/revisaoItens/RevisaoItemResponse.java
+++ src/main/java/com/jotanunes/especificacoes/dto/revisaoItens/RevisaoItemResponse.java
@@ -1,6 +1,7 @@
 package com.jotanunes.especificacoes.dto.revisaoItens;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.Date;
 
@@ -8,6 +9,7 @@ public record RevisaoItemResponse(
         Integer id,
         Integer itemId,
         String status,
+        @JsonInclude(JsonInclude.Include.NON_NULL)
         String motivo,
         String emailUsuario,
         @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
