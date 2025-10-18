907976ff71d91282c901c1c8616c08795c1757bb
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-10-18 18:13:51 -0300

    Documentação de controllers

--- src/main/java/com/jotanunes/especificacoes/controller/CatalogoAmbienteController.java
+++ src/main/java/com/jotanunes/especificacoes/controller/CatalogoAmbienteController.java
@@ -3,6 +3,8 @@ package com.jotanunes.especificacoes.controller;
 import com.jotanunes.especificacoes.dto.ambiente.CatalogoAmbienteRequest;
 import com.jotanunes.especificacoes.dto.ambiente.CatalogoAmbienteResponse;
 import com.jotanunes.especificacoes.service.CatalogoAmbienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/catalogo-ambientes")
+@Tag(name = "Catálogo de Ambientes", description = "Operações relacionadas ao catálogo de ambientes")
 public class CatalogoAmbienteController {
 
     private final CatalogoAmbienteService service;
@@ -19,11 +22,19 @@ public class CatalogoAmbienteController {
         this.service = service;
     }
 
+    @Operation(
+            summary = "Listar todos os ambientes do catálogo",
+            description = "Retorna uma lista de todos os ambientes disponíveis no catálogo."
+    )
     @GetMapping
     public List<CatalogoAmbienteResponse> getAllCatalogoAmbientes() {
         return service.getAllCatalogoAmbientes();
     }
 
+    @Operation(
+            summary = "Criar um novo ambiente no catálogo",
+            description = "Adiciona um novo ambiente ao catálogo de ambientes."
+    )
     @PostMapping
     public ResponseEntity<CatalogoAmbienteResponse> createCatalogoAmbiente(@RequestBody @Valid CatalogoAmbienteRequest request) {
         CatalogoAmbienteResponse response = service.createCatalogoAmbiente(request);
--- src/main/java/com/jotanunes/especificacoes/controller/CatalogoItemController.java
+++ src/main/java/com/jotanunes/especificacoes/controller/CatalogoItemController.java
@@ -3,6 +3,8 @@ package com.jotanunes.especificacoes.controller;
 import com.jotanunes.especificacoes.dto.item.CatalogoItemRequest;
 import com.jotanunes.especificacoes.dto.item.CatalogoItemResponse;
 import com.jotanunes.especificacoes.service.CatalogoItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/catalogo-itens")
+@Tag(name = "Catálogo de Itens", description = "Operações relacionadas ao catálogo de itens")
 public class CatalogoItemController {
 
     private final CatalogoItemService service;
@@ -19,11 +22,19 @@ public class CatalogoItemController {
         this.service = service;
     }
 
+    @Operation(
+            summary = "Listar todos os itens do catálogo",
+            description = "Retorna uma lista de todos os itens disponíveis no catálogo."
+    )
     @GetMapping
     public List<CatalogoItemResponse> getAll() {
         return service.getAll();
     }
 
+    @Operation(
+            summary = "Criar um novo item no catálogo",
+            description = "Adiciona um novo item ao catálogo de itens."
+    )
     @PostMapping
     public ResponseEntity<CatalogoItemResponse> create(@RequestBody @Valid CatalogoItemRequest request) {
         CatalogoItemResponse response = service.create(request);
