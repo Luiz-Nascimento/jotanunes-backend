94e6dbef3f79986d501864df5ca5a71246e3b086
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-10-18 18:13:51 -0300

    Documentação de controllers

--- src/main/java/com/jotanunes/especificacoes/controller/RevisaoItemController.java
+++ src/main/java/com/jotanunes/especificacoes/controller/RevisaoItemController.java
@@ -3,6 +3,8 @@ package com.jotanunes.especificacoes.controller;
 
 import com.jotanunes.especificacoes.dto.revisaoItens.RevisaoItemResponse;
 import com.jotanunes.especificacoes.service.RevisaoItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/revisoes-itens")
+@Tag(name = "Revisão de Itens", description = "Operações relacionadas à auditoria de revisão de itens")
 public class RevisaoItemController {
 
     private final RevisaoItemService service;
@@ -19,6 +22,10 @@ public class RevisaoItemController {
         this.service = service;
     }
 
+    @Operation(
+            summary = "Listar todas as revisões de itens",
+            description = "Retorna uma lista de todas as revisões de itens realizadas no sistema."
+    )
     @GetMapping
     public List<RevisaoItemResponse> listAll() {
         return service.findAll();
