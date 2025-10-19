a27104ba7be50bda53ef72cd57364ae596fcf7d6
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-10-19 17:07:34 -0300

    feat: Formatação de timestamp de exception response para horário local

--- src/main/java/com/jotanunes/especificacoes/exception/ExceptionResponse.java
+++ src/main/java/com/jotanunes/especificacoes/exception/ExceptionResponse.java
@@ -1,5 +1,12 @@
 package com.jotanunes.especificacoes.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
-public record ExceptionResponse(Date timestamp, String message, String details) {}
+
+public record ExceptionResponse(
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss", timezone = "America/Sao_Paulo")
+        Date timestamp,
+        String message,
+        String details) {}
