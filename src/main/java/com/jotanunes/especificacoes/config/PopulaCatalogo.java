f173ad9c8e7bf8062a01aab56b7951b21e3d2bfa
Adriano Fonseca <106631836+f0nseca7@users.noreply.github.com>
2025-11-05 08:48:10 -0300

    atualizado nomes de materiais e marcas

--- src/main/java/com/jotanunes/especificacoes/config/PopulaCatalogo.java
+++ src/main/java/com/jotanunes/especificacoes/config/PopulaCatalogo.java
@@ -24,24 +24,77 @@ public class PopulaCatalogo implements CommandLineRunner {
     public void run(String... args) {
         if (materialRepository.count() == 0) {
             materialRepository.saveAll(List.of(
-                    new Material("Madeira"),
-                    new Material("Aço Inox"),
-                    new Material("Vidro Temperado"),
-                    new Material("Alumínio"),
-                    new Material("Cimento")
+                    new Material("Cerâmica"),
+                    new Material("Laminado"),
+                    new Material("Esquadria"),
+                    new Material("Ferragem"),
+                    new Material("Inst. Elétrica"),
+                    new Material("Metal Sanitário"),
+                    new Material("Louças"),
+                    new Material("Porta (alumínio)"),
+                    new Material("Cuba (inox)"),
+                    new Material("Porcenalato"),
+                    new Material("Cuba (louça)")
             ));
-            System.out.println("✅ Materiais populados com sucesso!");
+            System.out.println("Materiais populados com sucesso!");
         }
 
         if (marcaRepository.count() == 0) {
             marcaRepository.saveAll(List.of(
-                    new Marca("Tigre"),
+                    new Marca("Aliança"),
+                    new Marca("Alumasa"),
+                    new Marca("Alumbra"),
+                    new Marca("Arielle"),
+                    new Marca("Atlantica"),
+                    new Marca("Biancogrês"),
+                    new Marca("Camelo Fior"),
+                    new Marca("Celite"),
+                    new Marca("Ceusa"),
                     new Marca("Deca"),
-                    new Marca("Amanco"),
+                    new Marca("Durafloor"),
+                    new Marca("Eliane"),
+                    new Marca("Elizabeth"),
+                    new Marca("Esaf"),
+                    new Marca("Eucatex"),
+                    new Marca("Fabrimar"),
+                    new Marca("Fame"),
+                    new Marca("Forusi"),
+                    new Marca("Frank"),
+                    new Marca("Ghel Plus"),
+                    new Marca("Ilumi"),
+                    new Marca("Incepa"),
+                    new Marca("Incesa"),
+                    new Marca("Kelly"),
+                    new Marca("La Fonte"),
+                    new Marca("Lef"),
+                    new Marca("Logasa"),
+                    new Marca("Margirius"),
+                    new Marca("Mari L"),
+                    new Marca("Mgm"),
+                    new Marca("Pado"),
+                    new Marca("Pamesa"),
+                    new Marca("Papaiz"),
+                    new Marca("Peesa"),
+                    new Marca("Pianox"),
+                    new Marca("Pointer"),
+                    new Marca("Portinari"),
                     new Marca("Portobello"),
-                    new Marca("Eliane")
+                    new Marca("Ramassol"),
+                    new Marca("Roca"),
+                    new Marca("Schneider"),
+                    new Marca("Silvana"),
+                    new Marca("Soprano"),
+                    new Marca("Stam"),
+                    new Marca("Steck"),
+                    new Marca("Talita"),
+                    new Marca("Tecnocuba"),
+                    new Marca("Tecnogres"),
+                    new Marca("Tramontina"),
+                    new Marca("Una Max"),
+                    new Marca("Unicasa"),
+                    new Marca("Villagres")
             ));
-            System.out.println("✅ Marcas populadas com sucesso!");
+            System.out.println("Marcas populadas com sucesso!");
         }
     }
 }
