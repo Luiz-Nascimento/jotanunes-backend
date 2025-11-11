bd40e6f0500c8553aa473b12e801a1f1f7c52aab
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-11-11 16:59:47 -0300

    feat: aplicando controle de concorrência no serviço de geração de documento

--- src/main/java/com/jotanunes/especificacoes/service/DocGenerateService.java
+++ src/main/java/com/jotanunes/especificacoes/service/DocGenerateService.java
@@ -2,6 +2,7 @@ package com.jotanunes.especificacoes.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jotanunes.especificacoes.dto.empreendimento.EspecificacaTecnicaDTO;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -33,64 +35,87 @@ public class DocGenerateService {
     @Value("${app.docs.timeout-seconds:30}")
     private int timeoutSeconds;
 
+    @Value("${app.docs.max-concurrent:5}")
+    private int maxConcurrent;
+
+    private Semaphore semaphore;
+
+    @PostConstruct
+    public void init() {
+        this.semaphore = new Semaphore(maxConcurrent);
+        logger.info("DocGenerateService inicializado com {} permissões de concorrência", maxConcurrent);
+    }
+
+
+
 
     public byte[] gerarDocx(EspecificacaTecnicaDTO dados) throws IOException, InterruptedException {
 
-        Path nodeDirectory = Paths.get(nodeProjectPath).toAbsolutePath().normalize();
-        if (!Files.isDirectory(nodeDirectory)) {
-            logger.error("Diretório do projeto node não encontrado: {}", nodeDirectory);
-            throw new IllegalStateException("Pasta '" + nodeProjectPath + "' não encontrada.");
+        logger.debug("Aguardando permissao para gerar DOCX... {} permissoes disponiveis", semaphore.availablePermits());
+
+        semaphore.acquire();
+
+        logger.debug("Permissão adquirida. Iniciando geração do DOCX.");
+        try {
+            Path nodeDirectory = Paths.get(nodeProjectPath).toAbsolutePath().normalize();
+            if (!Files.isDirectory(nodeDirectory)) {
+                logger.error("Diretório do projeto node não encontrado: {}", nodeDirectory);
+                throw new IllegalStateException("Pasta '" + nodeProjectPath + "' não encontrada.");
+            }
+
+            Path tempJson = Files.createTempFile("dados-", ".json");
+            Path tempDocx = Files.createTempFile("especificacao-", ".docx");
+
+            logger.debug("Arquivos temporários criados: JSON={}, DOCX={}", tempJson, tempDocx);
+
+            try {
+                Files.write(tempJson, objectMapper.writeValueAsBytes(dados));
+
+                ProcessBuilder pb = new ProcessBuilder(
+                        nodeCommand,
+                        nodeScriptname,
+                        tempJson.toString(),
+                        tempDocx.toString()
+                );
+
+                pb.directory(nodeDirectory.toFile());
+                pb.redirectErrorStream(true);
+
+                logger.info("Iniciando processo Node.js para gerar DOCX...");
+                Process process = pb.start();
+
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        logger.debug("[NodeJS]: {}", line);
+                    }
+                }
+
+                boolean finished = process.waitFor(timeoutSeconds, TimeUnit.SECONDS);
+
+                if (!finished || process.exitValue() != 0) {
+                    if (!finished) {
+                        process.destroyForcibly();
+                        logger.error("Time ao gerar DOCX após {}s", timeoutSeconds);
+                    } else {
+                        logger.error("Processo Node falhou com código de saída: {}", process.exitValue());
+                    }
+                    throw new RuntimeException("Falha na geração do DOCX. Verifique os logs para detalhes");
+                }
+                logger.info("DOCX gerado com sucesso!");
+                return Files.readAllBytes(tempDocx);
+
+            } finally {
+                try {
+                    Files.deleteIfExists(tempJson);
+                    Files.deleteIfExists(tempDocx);
+                } catch (IOException e) {
+                    logger.warn("Não foi possível limpar alguns arquivos temporários: {}", e.getMessage());
+                }
+            }
+        } finally {
+            semaphore.release();
+            logger.debug("Permissao liberada. {} permissoes disponiveis", semaphore.availablePermits());
         }
-
-        Path tempJson = Files.createTempFile("dados-", ".json");
-        Path tempDocx = Files.createTempFile("especificacao-", ".docx");
-
-        logger.debug("Arquivos temporários criados: JSON={}, DOCX={}", tempJson, tempDocx);
-
-       try {
-           Files.write(tempJson, objectMapper.writeValueAsBytes(dados));
-
-           ProcessBuilder pb = new ProcessBuilder(
-                   nodeCommand,
-                   nodeScriptname,
-                   tempJson.toString(),
-                   tempDocx.toString()
-           );
-
-           pb.directory(nodeDirectory.toFile());
-           pb.redirectErrorStream(true);
-
-           logger.info("Iniciando processo Node.js para gerar DOCX...");
-           Process process = pb.start();
-
-           try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-               String line;
-               while ((line = reader.readLine()) != null) {
-                   logger.debug("[NodeJS]: {}", line);
-               }
-           }
-
-           boolean finished = process.waitFor(timeoutSeconds, TimeUnit.SECONDS);
-
-           if (!finished || process.exitValue() != 0) {
-               if (!finished) {
-                   process.destroyForcibly();
-                   logger.error("Time ao gerar DOCX após {}s", timeoutSeconds);
-               } else {
-                   logger.error("Processo Node falhou com código de saída: {}", process.exitValue());
-               }
-               throw new RuntimeException("Falha na geração do DOCX. Verfique os logs para detalhes");
-           }
-           logger.info("DOCX gerado com sucesso!");
-           return Files.readAllBytes(tempDocx);
-
-       } finally {
-           try {
-               Files.deleteIfExists(tempJson);
-               Files.deleteIfExists(tempDocx);
-           } catch (IOException e) {
-               logger.warn("Não foi possível limpar alguns arquivos temporários: {}", e.getMessage());
-           }
-       }
     }
 }
