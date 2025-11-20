f2908ff3971670e1c4c6db0f42310be22b967925
Luiz Nascimento <luiznascimento.developer@gmail.com>
2025-11-19 22:17:36 -0300

    feat: Só pode gerar docx do empreendimento caso ele esteja aprovado

--- src/main/java/com/jotanunes/especificacoes/service/RelatorioService.java
+++ src/main/java/com/jotanunes/especificacoes/service/RelatorioService.java
@@ -3,7 +3,12 @@ package com.jotanunes.especificacoes.service;
 
 import com.jotanunes.especificacoes.dto.documento.DocumentoGeradoDTO;
 import com.jotanunes.especificacoes.dto.empreendimento.EspecificacaTecnicaDTO;
+import com.jotanunes.especificacoes.enums.EmpreendimentoStatus;
 import com.jotanunes.especificacoes.exception.DocumentGenerationException;
+import com.jotanunes.especificacoes.exception.EmpreendimentoBusinessLogicException;
+import com.jotanunes.especificacoes.exception.ResourceNotFoundException;
+import com.jotanunes.especificacoes.model.Empreendimento;
+import com.jotanunes.especificacoes.repository.EmpreendimentoRepository;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +19,22 @@ public class RelatorioService {
 
     private final EmpreendimentoService empreendimentoService;
     private final DocGenerateService docGenerateService;
+    private final EmpreendimentoRepository empreendimentoRepository;
 
-    public RelatorioService(EmpreendimentoService empreendimentoService, DocGenerateService docGenerateService) {
+    public RelatorioService(EmpreendimentoService empreendimentoService, DocGenerateService docGenerateService, EmpreendimentoRepository empreendimentoRepository) {
         this.empreendimentoService = empreendimentoService;
         this.docGenerateService = docGenerateService;
+        this.empreendimentoRepository = empreendimentoRepository;
     }
 
     private static final MediaType DOCX_MEDIA_TYPE = MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
 
     public DocumentoGeradoDTO gerarEspecificacaoTecnica(Integer id) {
-
+        Empreendimento empreendimento = empreendimentoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Empreendimento não encontrado com id: "+ id));
+        if (empreendimento.getStatus() != EmpreendimentoStatus.APROVADO) {
+            throw new EmpreendimentoBusinessLogicException("Empreendimento não aprovado! Falha na geração do documento");
+        }
         EspecificacaTecnicaDTO dados = empreendimentoService.getDadosParaRelatorio(id);
         try {
             byte[] docxBytes = docGenerateService.gerarDocx(dados);
