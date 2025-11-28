fae8db45fd4f60531d04ac92b508babfe24386c3
Luiz Nascimento <102400191+Luiz-Nascimento@users.noreply.github.com>
2025-11-28 16:21:34 -0300

    Feat/revisoes by empreendimento (#81)
    * feat: adicionado campo de getNome em item
    
    * feat: adicionado campo getNome em ambiente
    
    * feat: query para buscar revisões por id de empreendimento
    
    * feat: inclusão de campos nome item, nome ambiente no response de revisao
    
    * feat: adicionado toDtoList e ajustes para inclusão dos novos campos
    
    * feat: endpoint para visualizar revisões de itens por empreendimento

--- src/main/java/com/jotanunes/especificacoes/mapper/RevisaoItemMapper.java
+++ src/main/java/com/jotanunes/especificacoes/mapper/RevisaoItemMapper.java
@@ -5,10 +5,16 @@ import com.jotanunes.especificacoes.model.RevisaoItem;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface RevisaoItemMapper {
 
     @Mapping(source = "usuario.email", target = "emailUsuario")
     @Mapping(source = "item.id", target = "itemId")
+    @Mapping(source = "item.nome", target = "itemNome")
+    @Mapping(source = "item.ambiente.nome", target = "ambienteNome")
     RevisaoItemResponse toDto(RevisaoItem revisaoItem);
+
+    List<RevisaoItemResponse> toDtoList(List<RevisaoItem> revisaoItem);
 }
