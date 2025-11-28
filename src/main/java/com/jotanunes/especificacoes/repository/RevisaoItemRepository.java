19258c35745928a33bd16770c5837d1843c3fb09
Luiz Nascimento <luiznascimento.developer@gmail.com>
2025-11-28 16:19:21 -0300

    feat: query para buscar revisões por id de empreendimento

--- src/main/java/com/jotanunes/especificacoes/repository/RevisaoItemRepository.java
+++ src/main/java/com/jotanunes/especificacoes/repository/RevisaoItemRepository.java
@@ -4,6 +4,9 @@ import com.jotanunes.especificacoes.model.RevisaoItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RevisaoItemRepository extends JpaRepository<RevisaoItem, Integer> {
+    List<RevisaoItem> findByItemAmbienteEmpreendimentoId(Integer idEmpreendimento);
 }
