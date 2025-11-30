c633a10da1f13b6fc976ce9ebf6d056d684a9e93
Luiz Nascimento <102400191+Luiz-Nascimento@users.noreply.github.com>
2025-11-30 18:57:56 -0300

    Feat/motivo reprovacao get itens (#83)
    * feat: query customizada para buscar itens de um ambiente mais rápido
    
    * feat: inclusão de lista de revisões no item, método para pegar motivo da reprovação da ultima revisão, mudança no revisaoItem
    
    * feat: itemResponse agora retorna motivo da reprovação caso esteja reprovado

--- src/main/java/com/jotanunes/especificacoes/repository/ItemRepository.java
+++ src/main/java/com/jotanunes/especificacoes/repository/ItemRepository.java
@@ -2,12 +2,14 @@ package com.jotanunes.especificacoes.repository;
 
 import com.jotanunes.especificacoes.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
+    @Query("SELECT i FROM Item i LEFT JOIN FETCH i.historicoRevisoes WHERE i.ambiente.id = :id")
     List<Item> findByAmbienteId(Integer id);
     long countByAmbiente_Empreendimento_Id(Integer idEmpreendimento);
 }
